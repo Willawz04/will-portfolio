0213aa07ecde4f5cca0cdb0935f4427dd02ca1ed
Willawz04 <will.lawson.is@gmail.com>
2025-03-31 22:38:59 -0400

    Revert Next.js config to minimal working state

--- next.config.js
+++ next.config.js
@@ -4,21 +4,8 @@ const nextConfig = {
     config.module.rules.push({
       test: /\.(glb|gltf)$/,
       type: 'asset/resource',
-      generator: {
-        filename: 'static/chunks/[path][name][ext]'
-      }
     });
     return config;
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-        pathname: '/**',
-      },
-    ],
   },
 };
 
